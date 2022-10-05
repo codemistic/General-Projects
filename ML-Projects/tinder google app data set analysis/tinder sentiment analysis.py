9d48b77605d0c5b45a9738df9301e688a545d260
Muhammad Arslan Shahzad <69626496+muhammadarslanshahzad@users.noreply.github.com>
2022-10-06 01:05:09 +0500

    tinder sentiment analysis.py

---
+++ ML-Projects/tinder google app data set analysis/tinder sentiment analysis.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import nltk
+import re
+from nltk.corpus import stopwords
+import string
+
+df=pd.read_csv('./tinder_data/tinder_google_play_reviews.csv')
+
+df.head()
+
+data=df[['content']]
+data.head()
+
+data.isnull().sum()
+data=data.dropna()
+
+nltk.download('stopwords')
+stemmer=nltk.SnowballStemmer('english')
+stopword=set(stopwords.words('english'))
+
+def clean(text):
+    text=str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text = " ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text = " ".join(text)
+    return text
+
+data['content']=data['content'].apply(clean)
+
+text=' '.join(i for i in data.content)
+stopwords=set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure(figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["content"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["content"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["content"]]
+data = data[["content", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+positive =' '.join([i for i in data['content'][data['Positive'] > data["Negative"]]])
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(positive)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+negative =' '.join([i for i in data['content'][data['Negative'] > data["Positive"]]])
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(negative)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
+
+print("Positive: ", x)
+print("Negative: ", y)
+print("Neutral: ", z)
+
