4c1fb56544f0dbf20e40c3cc9db1552d3b9d94ee
sajalrasto <30717607+sajalrasto@users.noreply.github.com>
2022-10-09 17:05:47 +0530

    Update jenkins.sh

--- Terraform-scripts-AWS/jenkins.sh
+++ Terraform-scripts-AWS/jenkins.sh
@@ -14,9 +14,3 @@ sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install epel-release-latest-7.noarch.rpm
-sudo yum update -y
-sudo yum install python python-devel python-pip openssl ansible -y
-sudo amazon-linux-extras install ansible2
-# sudo ansible --version
