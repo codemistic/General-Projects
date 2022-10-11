43b0ca075c7802e55a0d9873f8058f42ae983983
apollo647 <72569185+apollo647@users.noreply.github.com>
2022-10-12 02:13:27 +0530

    Create face.py

---
+++ ML-Projects/face recoginition/face.py
@@ -0,0 +1,79 @@
+import face_recognition as fr
+import os
+import cv2
+import numpy as np
+
+
+def get_encoded_faces():
+    
+    encoded = {}
+
+    for dirpath, dnames, fnames in os.walk("./faces"):
+        for f in fnames:
+            if f.endswith(".jpg") or f.endswith(".png"):
+                face = fr.load_image_file("faces/" + f)
+                encoding = fr.face_encodings(face)[0]
+                encoded[f.split(".")[0]] = encoding
+
+    return encoded
+
+
+
+def classify_face(im):
+    
+    faces = get_encoded_faces()
+    faces_encoded = list(faces.values())
+    known_face_names = list(faces.keys())
+
+    img = cv2.imread(im, 1)
+    #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    #img = img[:,:,::-1]
+ 
+    face_locations = fr.face_locations(img)
+    unknown_face_encodings = fr.face_encodings(img, face_locations)
+
+    face_names = []
+    for face_encoding in unknown_face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = fr.compare_faces(faces_encoded, face_encoding)
+        name = "Unknown"
+
+        # use the known face with the smallest distance to the new face
+        face_distances = fr.face_distance(faces_encoded, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Draw a box around the face
+            cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
+
+
+    # Display the resulting image
+    while True:
+
+        cv2.imshow('Video', img)
+        return face_names 
+
+def learning(image):
+    c_face =classify_face(image)
+    if 'Unknown' in c_face:
+    que = input("There is an Unknown Face Would You like Add it to system ")
+    if que == 'yes' or que == "Yes":
+      imageFileName = input("enter the name of the image file: ")
+      shutil.move(imageFileName, './faces')
+      print(learning(image))
+    else:
+      print(c_face)
+  else:
+    print(c_face)
+    
+image = input("enter the name of the image file: ")    
+print(learning(image))
