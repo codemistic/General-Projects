4ea9a45d843f9660a3680cee8d80302ede4e9c78
Heart over Mind <rishikeshgautam59@gmail.com>
2022-10-17 16:42:04 +0545

    Added two C projects hangman and interest

---
+++ C Project/Interest Calculator/interest.c
@@ -0,0 +1,46 @@
+// Calculate simple interest and compound amount with menu
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int choice;
+    float p, r, t, si, ci;
+    printf("1. Simple Interest\n");
+    printf("2. Compound Amount\n\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch(choice){
+        
+        case 1:
+            printf("Enter the principal amount: ");
+            scanf("%f", &p);
+            printf("Enter the rate of interest: ");
+            scanf("%f", &r);
+            printf("Enter the time period: ");
+            scanf("%f", &t);
+            
+            si = (p * r * t) / 100;
+            printf("Simple interest = %.2f", si);
+            break;
+        
+        case 2:
+            printf("Enter the principal amount: ");
+            scanf("%f", &p);
+            printf("Enter the rate of interest: ");
+            scanf("%f", &r);
+            printf("Enter the time period: ");
+            scanf("%f", &t);
+            
+            ci = p * pow((1 + r / 100), t);
+            printf("Compound amount = %.2f", ci);
+            break;
+        
+        default:
+            printf("Invalid choice!");
+    }
+    return 0;
+}
+
+    
