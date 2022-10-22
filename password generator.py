cd267d8394230baa8de88f04dabd44c45911e11d
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2022-10-22 15:49:00 +0530

    Delete password generator.py

--- password generator.py
+++
@@ -1,5 +0,0 @@
-import random
-passlen = int(input("enter the length of password"))
-s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(s,passlen ))
-print(p)
