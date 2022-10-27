a4f068b03b92d8c1476d1c8b653a2e8b725183a5
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-10-27 22:29:45 +0530

    cryptocurrency tracker added (#357)
    * cryptocurrency tracker added
    
    * twitter hatespeech recognition

---
+++ cryptohunter/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import {BrowserRouter,Route} from 'react-router-dom';
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./pages/CoinPage";
+import {makeStyles} from "@material-ui/core";
+
+function App() {
+  const useStyles=makeStyles( ()=> ({
+    App:{
+      backgroundColor:"#14161a",
+      color:"white",
+      minHeight:"100vh"
+    }
+  }))
+
+  const classes=useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <div>
+          <Header />
+          <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
