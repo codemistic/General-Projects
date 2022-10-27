e89348537f32ce3746aac624f7e6d1ad08040099
Sumanth Polisetty <sumanth472@gmail.com>
2022-10-27 22:27:19 +0530

    Regression Model using Tensorflow and Deep Neural Networks (#360)

---
+++ DNN_Regression_TF/README.md
@@ -0,0 +1,87 @@
+<div align="center" id="top"> 
+  <!-- <img src="./.github/app.gif" alt="Regression Model using Tf" /> -->
+
+  &#xa0;
+
+  <!-- <a href="https://regressiontf.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">Regression Model using Tensorflow</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/sumanthp/General-Projects?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/sumanthp/General-Projects?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/sumanthp/General-Projects?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/sumanthp/General-Projects?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/sumanthp/General-Projects?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/sumanthp/General-Projects?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/sumanthp/General-Projects?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  Regression Model using  Tf 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="https://github.com/sumanthp" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Tensorflow is used to create regression model using deep neural networks
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [Tensorflow](https://www.tensorflow.org/)
+- [Python](https://www.python.org/)
+- [Numpy](https://numpy.org/)
+- [Matplotlib](https://matplotlib.org/)
+- [Scikit-Learn](https://scikit-learn.org/stable/index.html)
+
+## :white_check_mark: Requirements ##
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Python](https://www.python.org/downloads/) installed.
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/sumanthp/General-Projects
+
+# Access
+$ cd General-Projects
+
+# Install dependencies
+$ pip install -r requirements.txt
+
+# Run the project
+$ python regression.py
+
+# The python script will execute in terminal
+```
+
+
+
+Made with :heart: by <a href="https://github.com/sumanthp" target="_blank">Sumanth</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ DNN_Regression_TF/regression.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# Make NumPy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True)
+dataset = raw_dataset.copy()
+dataset.tail()
+dataset = dataset.dropna()
+dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
+dataset = pd.get_dummies(dataset, columns=['Origin'], prefix='', prefix_sep='')
+dataset.tail()
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
+train_dataset.describe().transpose()
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('MPG')
+test_labels = test_features.pop('MPG')
+train_dataset.describe().transpose()[['mean', 'std']]
+normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer.adapt(np.array(train_features))
+
+def build_and_compile_model(norm):
+  model = keras.Sequential([
+      norm,
+      layers.Dense(64, activation='relu'),
+      layers.Dense(64, activation='relu'),
+      layers.Dense(1)
+  ])
+
+  model.compile(loss='mean_absolute_error',
+                optimizer=tf.keras.optimizers.Adam(0.001))
+  return model
+
+horsepower = np.array(train_features['Horsepower'])
+horsepower_normalizer = layers.Normalization(input_shape=[1,], axis=None)
+horsepower_normalizer.adapt(horsepower)
+dnn_horsepower_model = build_and_compile_model(horsepower_normalizer)
+dnn_horsepower_model.summary()
+history = dnn_horsepower_model.fit(
+    train_features['Horsepower'],
+    train_labels,
+    validation_split=0.2,
+    verbose=0, epochs=100)
+
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.ylim([0, 10])
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [MPG]')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+
+plot_loss(history)
+test_results = {}
+test_results['dnn_horsepower_model'] = dnn_horsepower_model.evaluate(
+    test_features['Horsepower'], test_labels,
+    verbose=0)   
+
+dnn_model = build_and_compile_model(normalizer)
+dnn_model.summary()
+history = dnn_model.fit(
+    train_features,
+    train_labels,
+    validation_split=0.2,
+    verbose=0, epochs=100)
+plot_loss(history)
+test_results['dnn_model'] = dnn_model.evaluate(test_features, test_labels, verbose=0)
+df = pd.DataFrame(test_results, index=['Mean absolute error [MPG]']).T
+print(df)
+
+test_predictions = dnn_model.predict(test_features).flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+lims = [0, 50]
+plt.xlim(lims)
+plt.ylim(lims)
+_ = plt.plot(lims, lims)
+plt.show()
+error = test_predictions - test_labels
+plt.hist(error, bins=25)
+plt.xlabel('Prediction Error [MPG]')
+_ = plt.ylabel('Count')
+plt.show()
+dnn_model.save('dnn_model')
